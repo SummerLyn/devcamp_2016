123a351d946866a7a11b0e785e235efd7d6b37a7
SummerLyn <s.lynbryant@gmail.com>
2016-12-27 16:26:55 -0800

    JavaScript Assignments

---
+++ Assignments/JavaScript/adding_element_ul/script_2.js
@@ -0,0 +1,12 @@
+// Create the following program
+// Ask the user if they want to create an ordered or unordered list for their favorite foods.
+
+// Then create the html element dynamically by asking for their five favorite foods and add them to the list.
+
+
+//STEP 2 create an array in your javascript file that has five of your favorite foods.
+// redo your program so if the user enters a food that is also on your favorite food list then make the font color green
+// if it is not on your list make the font color red
+
+
+var myFavoriteFoodList = ["bananas", "apples", "pie", "tacos", "crumpets"];
---
+++ Assignments/JavaScript/randombox/randomBoxColor.js
@@ -0,0 +1,25 @@
+
+
+function makeRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() *16)];
+    }
+    return color;
+
+
+}
+
+function changeColor() {
+
+    document.getElementById('box1').style.backgroundColor = makeRandomColor();
+    document.getElementById('box2').style.backgroundColor = makeRandomColor();
+    document.getElementById('box3').style.backgroundColor = makeRandomColor();
+    document.getElementById('box4').style.backgroundColor = makeRandomColor();
+    document.getElementById('box5').style.backgroundColor = makeRandomColor();
+    document.getElementById('box6').style.backgroundColor = makeRandomColor();
+
+}
+
+changeColor();
